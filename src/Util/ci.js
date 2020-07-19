dfde5d8fc2a11bcb98a9fe4886ac2985dfcf222a
xhayper <ikq31421@gmail.com>
2020-07-19 17:05:04 +0700

    Updated Stuff

--- src/Util/ci.js
+++ src/Util/ci.js
@@ -5,8 +5,14 @@ THIS FILE WAS BUILT FOR CI TO TEST IF THE SOURCE IS FUNCTION CORRECTLY.
 require("dotenv").config();
 
 let Discord = require("discord.js-light"),
+    os = require("os"),
     main = require("../main.js"),
-    client = new Discord.Client({disableMentions : "everyone"});
+    client = new Discord.Client({disableMentions : "everyone"}),
+    osType = {
+        "Linux": "Linux",
+        "Darwin": "macOS",
+        "Windows_NT": "Windows"
+    };
 
 client.on("debug", (debug) => {
     let censoredToken = debug.split(" ")[2].replace(/\*/g, "");
@@ -16,19 +22,19 @@ client.on("debug", (debug) => {
 
 client.on("ready", () => {
     main = null;
-    console.log("[CI] Sending a test webhook.")
+    console.log(`[${osType[os.type()]} CI] Sending a test webhook.`)
     let webhook = new Discord.WebhookClient(process.env.WEBHOOK_ID, process.env.WEBHOOK_TOKEN);
-    webhook.send("CI's Webhook Test", {
+    webhook.send(`${osType[os.type()]} CI's Test Webhook`, {
             username: client.user.username,
             avatarURL: client.user.avatarURL({dynamic: true, size: 4096})
         }).then(() => {
             client = null;
-            console.log("[CI] Test successfully, Exiting with status code: 0")
+            console.log(`[${osType[os.type()]} CI] Test successfully, Exiting with status code: 0`);
             process.exit(0);
         }).catch((e) => {
         console.error(e);
         client = null;
-        console.log("[CI] Test unsuccessfully, Exiting with status code: 1")
+        console.log(`[${osType[os.type()]} CI] Test unsuccessfully, Exiting with status code: 1`);
         return process.exit(1);
     });
 });
